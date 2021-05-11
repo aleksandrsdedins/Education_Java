cfa0feaac0e351790df64406dd391e55ebcd71d2
Aleksandrs Dedins <aleksandrsdedins@gmail.com>
2021-05-11 16:50:11 +0300

    Hello World!

---
+++ src/introduction/HelloWorld.java
@@ -0,0 +1,10 @@
+package introduction;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // This is my 1st Java Program
+        System.out.println("Hello World!");
+
+    }
+}
