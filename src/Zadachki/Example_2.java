46eba70c95e415fe74e676f02c8b95c8219096dc
Aleksandrs Dedins <aleksandrsdedins@gmail.com>
2021-05-17 11:44:28 +0300

    Example 2 Done

--- src/Zadachki/Example_2.java
+++ src/Zadachki/Example_2.java
@@ -6,10 +6,12 @@ public class Example_2 {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
         String text = userInput.nextLine();
-        char [] arr = text.toCharArray();
+        char[] arr = text.toCharArray();
 
-
-
-
+        String output = "";
+        for (int i = arr.length - 1; i >= 0; i--) {
+            output += arr[i];
+        }
+        System.out.println(output);
     }
 }
